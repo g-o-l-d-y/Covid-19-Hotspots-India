8edeae4620fe4221ab2564e8f17697822145007e
Goldy Jeyarani R <43851318+g-o-l-d-y@users.noreply.github.com>
2021-03-14 15:02:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from flask import Flask
+import folium
+# importing the required libraries
+import pandas as pd
+
+# Visualisation libraries
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request
+
+
+# Manipulating the default plot size
+plt.rcParams['figure.figsize'] = 10, 12
+
+# Disable warnings 
+import warnings
+warnings.filterwarnings('ignore')
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+
 
 app=Flask(__name__)
 
